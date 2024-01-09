5caebeddcebeb2b1ec678132bfa61cbad32e5d33
Dev-Mohit-1220 <149872549+Dev-Mohit-1220@users.noreply.github.com>
2024-01-09 15:15:19 +0530

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router";
+
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log("Server running at port 3000");
+});
+
+// Mongo DB Connection string
+const MONGO_URL = process.env.ConnectionString;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL); // Connect Mongo DB with connection string
+mongoose.connection.on('error', (error: Error) => console.log(error)); // Use call back here, If error comes it will display on console
+
+app.use('/', router());

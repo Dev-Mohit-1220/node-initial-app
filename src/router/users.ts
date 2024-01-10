0ac58e04ca551ed2dcd567fc77348d0a3d796e57
Mohit Jogiwala <dev.mike.1220@gmail.com>
2024-01-10 18:24:43 +0530

    [Implemented]: is authenticated and is owner on routing

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { getAllUsers, deleteUser } from "../controllers/user"; // imported from the user controller
+import { isAuthenticated, isOwner } from "middlewares";
 
 export default (router: express.Router) => {
-  router.get("/users", getAllUsers);
-  router.delete("/users/:id", deleteUser);
+  router.get("/users", isAuthenticated, getAllUsers);
+  router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
 };
